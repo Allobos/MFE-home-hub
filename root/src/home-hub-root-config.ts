e4447dd85a32f0b1144f2a163c5d1ed96b5c8f3e
Jonnie Allobos <jonnieallobos@gmail.com>
2024-12-21 18:22:18 -0300

    feat: create root micro-frontend

---
+++ root/src/home-hub-root-config.ts
@@ -0,0 +1,12 @@
+import { LifeCycles, registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@home-hub/react-navbar",
+  app: () => System.import<LifeCycles>("@home-hub/react-navbar"),
+  // activeWhen: ["/"],
+  activeWhen: (location) => location.pathname === '/'
+});
+
+start({
+  urlRerouteOnly: true,
+});
